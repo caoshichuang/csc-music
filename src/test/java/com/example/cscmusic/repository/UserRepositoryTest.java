d0546d06826bb19b34b65757aade6e19a6328ab5
caoshichuang <18713973392@163.com>
2022-05-12 17:42:27 +0800

    fix: add

--- src/test/java/com/example/cscmusic/repository/UserRepositoryTest.java
+++ src/test/java/com/example/cscmusic/repository/UserRepositoryTest.java
@@ -13,25 +13,4 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class UserRepositoryTest {
-
-    @Autowired
-    UserRepository repository;
-
-    @Test
-    void findByUsername() {
-        User user = new User();
-        user.setUsername("liming");
-        user.setNickname("cc");
-        user.setEnabled(true);
-        user.setGender(Gender.FEMALE);
-        user.setLocked(false);
-        user.setPassword("123123124");
-        user.setLastLoginIp("127.0.0.1");
-        user.setLastLoginTime(new Date());
-//
-//        User saved = repository.save(user);
-        User result = repository.getByUsername("liming");
-        System.out.println(result.toString());
-
-    }
 }
